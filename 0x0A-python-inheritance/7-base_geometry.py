8e1b19c8d876388a584417e3859a20542cae954b
MrBoks <get2noahodogg@yahoo.com>
2023-02-19 13:53:07 +0000

    Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
+"""Class BaseGeometry"""
 
 
-class BaseGeometry():
-    """for use with shapes. Super class.
-    """
-
+class BaseGeometry:
     def area(self):
-        """instance method to calculate area of shape
-        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates integer input
-        """
-        if type(value) != int:
-            raise TypeError(name + " must be an integer")
-        elif value <= 0:
-            raise ValueError(name + " must be greater than 0")
+        """ Public instance method that validates value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
